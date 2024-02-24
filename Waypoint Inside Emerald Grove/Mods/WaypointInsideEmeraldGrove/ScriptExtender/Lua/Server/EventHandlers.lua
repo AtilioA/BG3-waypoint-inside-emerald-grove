6d90aa6c431b98d09151247ee7de4fc27347790d
Atílio <atiliodadalto@protonmail.com>
2024-02-23 23:46:24 -0300

    fix: handle region swap rejection and teleportation issue

--- Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Server/EventHandlers.lua
+++ Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Server/EventHandlers.lua
@@ -2,26 +2,54 @@ EHandlers = {}
 
 -- Flag set when a character has entered The Hollow
 EHandlers.HasEnteredGroveFlagGUID = "18cfaaeb-c0df-46ac-962d-0c300f816d73" -- DEN_General_State_EnteredCave
+-- Avoid teleporting/teleport back if the region swap was rejected
+EHandlers.RegionSwapRejected = false
+EHandlers.OriginalPosition = nil
 
 function HasCharacterEverEnteredGrove(character)
   return Osi.GetFlag(EHandlers.HasEnteredGroveFlagGUID, character) == 1
 end
 
 function ShouldTeleportToGrove(character)
+  local x, y, z = Osi.GetPosition(character)
+  EHandlers.OriginalPosition = { character = character, x = x, y = y, z = z }
+
   local shouldSneakingKeepDestination = JsonConfig.FEATURES.original_waypoint_if_sneaking and Utils.IsSneaking(character)
-  if not HasCharacterEverEnteredGrove(character) and not shouldSneakingKeepDestination then
-    Utils.DebugPrint(1, "Character has not entered grove yet or is sneaking, not teleporting")
+  if not HasCharacterEverEnteredGrove(character) or shouldSneakingKeepDestination then
+    Utils.DebugPrint(2, "Character has not entered grove yet or is sneaking, not teleporting")
     return false
+  elseif EHandlers.RegionSwapRejected then
+    return false
+  else
+    Utils.DebugPrint(2, "Character has entered grove or is not sneaking, teleporting")
+    EHandlers.RegionSwapRejected = false
+    return true
   end
 end
 
 function EHandlers.OnTeleportToWaypoint(character, trigger)
   Utils.DebugPrint(2, "OnTeleportToWaypoint: " .. character .. " " .. trigger)
   if trigger == Teleporting.EMERALD_GROVE_ENVIRONS_TRIGGER and ShouldTeleportToGrove(character) then
-    Utils.DebugPrint(1, "Teleporting to Emerald Grove")
     DelayedCall(100, function()
       Teleporting.TeleportToEmeraldGrove(character)
     end)
+  end
+
+  EHandlers.RegionSwapRejected = false
+end
+
+function EHandlers.OnReadyCheckFailed(id)
+  -- Should be triggered even if the target isn't Environs, BUT it works, so...
+  if id == "WaypointTravel_RegionSwap" then
+    Utils.DebugPrint(1, "Region swap was rejected, teleporting back to original position.")
+    if EHandlers.OriginalPosition then
+      local character, x, y, z = EHandlers.OriginalPosition.character, EHandlers.OriginalPosition.x,
+          EHandlers.OriginalPosition.y, EHandlers.OriginalPosition.z
+      Osi.TeleportToPosition(character, x, y, z, "TeleportBackAfterRefusedRegionSwap",
+        1,
+        1,
+        1, 0, 1)
+    end
   end
 end
 
--- Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Server/SubscribedEvents.lua
+++ Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Server/SubscribedEvents.lua
@@ -2,11 +2,9 @@ local function SubscribeToEvents()
   if JsonConfig.GENERAL.enabled == true then
     Utils.DebugPrint(2, "Subscribing to events with JSON config: " .. Ext.Json.Stringify(JsonConfig, { Beautify = true }))
 
-    Ext.Osiris.RegisterListener("TeleportToWaypoint", 2, "before", EHandlers.OnTeleportToWaypoint)
-    -- Ext.Osiris.RegisterListener("GainedControl", 1, "after", function(target)
-    --   local x, y, z = Osi.GetPosition(target)
-    --   Utils.DebugPrint(2, target .. " x: " .. x .. " y: " .. y .. " z: " .. z)
-    -- end)
+    Ext.Osiris.RegisterListener("TeleportToWaypoint", 2, "after", EHandlers.OnTeleportToWaypoint)
+
+    Ext.Osiris.RegisterListener("ReadyCheckFailed", 1, "after", EHandlers.OnReadyCheckFailed)
   end
 end
 
