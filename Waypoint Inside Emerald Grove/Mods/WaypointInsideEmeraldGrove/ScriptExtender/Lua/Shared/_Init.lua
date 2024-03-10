f4457f3d9a9b32599a9afe3870413ed1905785f1
Atílio <atiliodadalto@protonmail.com>
2024-03-09 21:09:32 -0300

    refactor: add shared helper scripts and update version print statements

--- Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Shared/_Init.lua
+++ Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Shared/_Init.lua
@@ -1,24 +1,25 @@
 setmetatable(Mods.WaypointInsideEmeraldGrove, { __index = Mods.VolitionCabinet })
 
+Ext.Require("Shared/Helpers/Config.lua")
+Ext.Require("Shared/Helpers/Printer.lua")
 Ext.Require("Shared/Utils.lua")
 Ext.Require("Shared/Helpers/String.lua")
--- Ext.Require("Shared/Helpers/Printer.lua")
-Ext.Require("Shared/Config.lua")
 Ext.Require("Shared/Teleporting.lua")
 -- Ext.Require("Shared/Helpers/Inventory.lua")
 Ext.Require("Shared/EventHandlers.lua")
 
-MOD_UUID = "e342ee75-f7c9-4aeb-b6de-403991578337"
-local MODVERSION = Ext.Mod.GetMod(MOD_UUID).Info.ModVersion
+local MODVERSION = Ext.Mod.GetMod(ModuleUUID).Info.ModVersion
+
 
 if MODVERSION == nil then
-    VolitionCabinetPrinter:PrintDebug("loaded (version unknown)")
+    WIEGWarn(0, "loaded (version unknown)")
 else
     -- Remove the last element (build/revision number) from the MODVERSION table
     table.remove(MODVERSION)
 
     local versionNumber = table.concat(MODVERSION, ".")
-    VolitionCabinetPrinter:PrintDebug("version " .. versionNumber .. " by Volitio loaded")
+    WIEGPrint(0, "Waypoint Inside Emerald Grove version " .. versionNumber .. " loaded")
+    WIEGPrint(2, "Config loaded: " .. Ext.Json.Stringify(Config:cfg(), { Beautify = true }))
 end
 
 local EventSubscription = Ext.Require("Shared/SubscribedEvents.lua")
