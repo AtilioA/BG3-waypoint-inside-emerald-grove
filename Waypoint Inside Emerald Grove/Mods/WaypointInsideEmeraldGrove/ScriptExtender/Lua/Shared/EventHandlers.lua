23e0be77e209c0ce28a3541906f769399d3bb79b
Atílio <atiliodadalto@protonmail.com>
2024-03-09 21:18:34 -0300

    feat/fix: add check for Ritual of Thorns activation and log teleport logic

--- Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Shared/EventHandlers.lua
+++ Waypoint Inside Emerald Grove/Mods/WaypointInsideEmeraldGrove/ScriptExtender/Lua/Shared/EventHandlers.lua
@@ -2,6 +2,7 @@ EHandlers = {}
 
 -- Flag set when a character has entered The Hollow
 EHandlers.HasEnteredGroveFlagGUID = "18cfaaeb-c0df-46ac-962d-0c300f816d73" -- DEN_General_State_EnteredCave
+
 -- Avoid teleporting/teleport back if the region swap was rejected
 EHandlers.RegionSwapRejected = false
 EHandlers.OriginalPosition = nil
@@ -10,18 +11,41 @@ function HasCharacterEverEnteredGrove(character)
   return Osi.GetFlag(EHandlers.HasEnteredGroveFlagGUID, character) == 1
 end
 
+function HasRitualOfThornsBeenActivated(character)
+  return Osi.GetFlag("DEN_Lockdown_State_Active_0b54c7d2-b7b1-4d0f-b8e4-0cf1ee32b1eb", character)
+end
+
+--- Check flags/DBs for whether the Grove lockdown has happened already
+---@param character GUIDSTRING The character to check against (won't even be used for DB check)
+function ShouldFlagsBlockTeleport(character)
+  local reason = ""
+  if HasCharacterEverEnteredGrove(character) == 0 then
+    reason = "Character has not entered grove yet"
+  elseif HasRitualOfThornsBeenActivated(character) == 1 then
+    reason = "Lockdown has happened"
+  end
+
+  return HasCharacterEverEnteredGrove(character) == 0 or HasRitualOfThornsBeenActivated(character) == 1, reason
+end
+
 function ShouldTeleportToGrove(character)
   local x, y, z = Osi.GetPosition(character)
   EHandlers.OriginalPosition = { character = character, x = x, y = y, z = z }
 
   local shouldSneakingKeepDestination = JsonConfig.FEATURES.original_waypoint_if_sneaking and Utils.IsSneaking(character)
-  if not HasCharacterEverEnteredGrove(character) or shouldSneakingKeepDestination then
-    Utils.DebugPrint(2, "Character has not entered grove yet or is sneaking, not teleporting")
+  local shouldBlockTeleporting, reason = ShouldFlagsBlockTeleport(character)
+  if shouldBlockTeleporting or shouldSneakingKeepDestination then
+    if shouldSneakingKeepDestination then
+      reason = "Character is sneaking"
+    end
+
+    Utils.DebugPrint(2, "Not teleporting. Reason: " .. reason)
     return false
   elseif EHandlers.RegionSwapRejected then
     return false
   else
-    Utils.DebugPrint(2, "Character has entered grove and is not sneaking, teleporting")
+    Utils.DebugPrint(2,
+      "Character has entered grove, lockdown has not happened, and character is not sneaking, teleporting")
     EHandlers.RegionSwapRejected = false
     return true
   end
